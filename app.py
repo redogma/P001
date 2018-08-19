15d3e6d01e9c7a7e766c1ee62c00cb213fee7f01
reddogma <id247528@gmail.com>
2018-08-19 16:14:52 +0100

    prep

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+
+from flask import Flask, jsonify, abort, make_response, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/test')
-def hello():
+def root():
     return 'P001!'
+
+@app.route('/home/')
+@app.route('/home/<name>')
+def home(name=None):
+	return render_template('home.html', name=name)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
