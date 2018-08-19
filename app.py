701c2c1254aef88ea8e0593a010026117ff03853
redogma <40769047+redogma@users.noreply.github.com>
2018-08-19 15:35:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/test')
 def hello():
-    return 'Hello World!'
+    return 'P001!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
