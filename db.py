e64446c787aab6fdf34af4378efbbc54c3970701
redogma <40769047+redogma@users.noreply.github.com>
2018-08-20 15:30:38 +0100

    Update db.py

--- db.py
+++ db.py
@@ -10,13 +10,30 @@ conn.set_isolation_level(0)
 cur = conn.cursor()
 
 queries = ["""
-            CREATE TABLE elevation (e_id INTEGER, e_name VARCHAR(255), e_elevation INTEGER)
+            CREATE TABLE elevation (e_id INTEGER PRIMARY KEY, e_pos INTEGER, e_name VARCHAR(100), e_elevation INTEGER)
             """,
            """
             CREATE TABLE run (r_id INTEGER, r_name VARCHAR(255), r_result VARCHAR(20))
+           """,
+           """
+             CREATE TABLE reference (ref_id SERIAL PRIMARY KEY, 
+                                     ref_key VARCHAR(20), 
+                                     ref_value BLOB, 
+                                     ref_modified_date VARCHAR(30))
+           """,
+           """
+              INSERT INTO reference (ref_key, ref_value) VALUES (\'NAME\', \'P001\')
+           """,
+           """
+              INSERT INTO elevation VALUES (1,1,\'Test\', 100)
+           """,
+           """
+              INSERT INTO run VALUES (1,\'Test\', \'30:01\')
            """
           ]
 for q in queries:
+  print('** Starting: %s' % (q,))
   cur.execute(q)
 
+print('** Finished running queries')
 conn.commit()  
