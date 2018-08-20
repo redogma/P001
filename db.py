c73bc438b4fe187db51f6b6b134c11301e9e2b23
redogma <40769047+redogma@users.noreply.github.com>
2018-08-20 13:43:16 +0100

    Create db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+import psycopg2
+import os
+import urlparse
+
+urlparse.uses_netloc.append('postgres')
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+conn = psycopg2.connect("dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname))
+cur = conn.cursor()
+
+query = "SELECT 1"
+cur.execute(query)
