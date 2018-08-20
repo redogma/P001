5522f4a597732e6aa2fe9d3d044fc57981a18962
redogma <40769047+redogma@users.noreply.github.com>
2018-08-20 14:04:36 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 import psycopg2
 import os
-import urlparse
+from urllib.parse import urlparse
 
-urlparse.uses_netloc.append('postgres')
+'urlparse.uses_netloc.append('postgres')
 url = urlparse.urlparse(os.environ['DATABASE_URL'])
 
 conn = psycopg2.connect("dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname))
