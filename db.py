fcbf1784570e4709c6925bacf4c0e2dd2f79b9f5
redogma <40769047+redogma@users.noreply.github.com>
2018-08-20 14:51:57 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+
 import psycopg2
 import os
 from urllib.parse import urlparse
@@ -6,7 +7,17 @@ from urllib.parse import urlparse
 url = urlparse(os.environ['DATABASE_URL'])
 
 conn = psycopg2.connect("dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname))
+conn.set_isolation_level(0)
 cur = conn.cursor()
 
-query = "SELECT 1"
-cur.execute(query)
+queries = ["""
+            CREATE TABLE elevation (e_id INTEGER, e_name VARCHAR(255), e_elevation INTEGER)
+            """,
+           """
+            CREATE TABLE run (r_id INTEGER, r_name VARCHAR(255), r_result VARCHAR(20))
+           """
+          ]
+for q in queries
+  cur.execute(query)
+
+conn.commit()  
